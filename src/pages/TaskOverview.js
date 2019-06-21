8cc0292f0d73c9429749d96d41cf9c78b9fe4225
Cam Carter <contact@camcarter.com>
2019-06-21 10:37:09 -0400

    Cleaned up context consumers a wee bit

--- src/pages/TaskOverview.js
+++ src/pages/TaskOverview.js
@@ -1,29 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import _ from 'lodash'
 import TaskContext from '../context/TaskContext'
 import TaskGroup from '../components/TaskGroup'
 
 const TaskOverview = () => {
+  const { tasks, _completeTask } = useContext(TaskContext)
   return (
-    <TaskContext.Consumer>
-      {context => (
-        <div className="container">
-          <h1 className="is-size-1">Things To Do</h1>
-          <hr/>
-          {
-            _.chain(context.tasks)
-             .groupBy('group')
-             .toPairs()
-             .map(([group, tasks]) => {
-               return (
-                 <TaskGroup key={group} group={group} tasks={tasks} />
-               )
-             })
-             .value()
-          }
-        </div>
-      )}
-    </TaskContext.Consumer>
+    <div className="container">
+      <h1 className="is-size-1">Things To Do</h1>
+      <hr/>
+      {
+        _.chain(tasks)
+         .groupBy('group')
+         .toPairs()
+         .map(([group, tasks]) => {
+           return (
+             <TaskGroup key={group} group={group} tasks={tasks} />
+           )
+         })
+         .value()
+      }
+    </div>
   )
 }
 
